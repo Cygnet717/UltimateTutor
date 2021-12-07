5d40754f6479e90fe4ad12d84b796dc584bb7391
Cygnet717 <kathybradt717@gmail.com>
2021-12-07 11:51:32 -0600

    make new deck version route, add card to deck route, and save current version routes done

--- server/controllers/deck-controller.js
+++ server/controllers/deck-controller.js
@@ -2,20 +2,21 @@ const { Deck, User } = require('../models');
 
 
 module.exports = {
-  async getUserDecks(req, res) { //get all decks by user
+  //get all decks by user
+  async getUserDecks(req, res) { // send "user_id" in body
     const foundDecks = await User.findOne(
       {_id: req.body.user_id}
     )
       .populate('decks');
 
-    res.json(foundDecks);
+    res.json(foundDecks.decks);
   },
 
-  async newVersion(req, res) {
-    res.json('new version')
-  },
-
-  async createDeck(req, res) {  //make new deck
+  //make new deck
+      // send "deckName", "user_id", "format"
+  //or to make new version
+      // send "deckName", "user_id", "versionToBe"++, "format", "deckCards" and "sideboard" array minus "_id" for each card
+  async createDeck(req, res) {  
     const newDeck = await Deck.create(req.body)
  
     const userOfDeck = await User.findOneAndUpdate(
@@ -27,28 +28,41 @@ module.exports = {
     res.json(userOfDeck)
   },
 
-  async updateDeck(req, res) {  //add card to deck/sideboard
+  //add card to deck/sideboard
+  async addToDeck(req, res) {  // send "deck_id", "cardName", "image", "cardType", "commander"boolean, "sideboard"boolean
     let updatedDeck;
+    const newCard = {
+      cardName: req.body.cardName,
+      image: req.body.image,
+      cardType: req.body.cardType,
+      commander: req.body.commander
+    }
     if(req.body.sideboard){
       updatedDeck = await Deck.findOneAndUpdate(
         {_id: req.body.deck_id},
-        {$push: {sideBoard: req}},
+        {$push: {sideBoard: newCard}},
         {new: true}
       )
     } else {
       updatedDeck = await Deck.findOneAndUpdate(
         {_id: req.body.deck_id},
-        {$push: {deckCards: req}},
+        {$push: {deckCards: newCard}},
         {new: true}
       )
     }
     
-
     res.json(updatedDeck)
   },
 
-  async saveDeckVersion(req, res) {
-    res.json('save deck version')
-  }
+  //save current version of deck
+  async saveCurrentVersion(req, res) {  // send "deck_id", "versionToBe"
+    const savedVersion = await Deck.findOneAndUpdate(
+      {_id: req.body.deck_id},
+      {version: req.body.versionToBe},
+      {new: true}
+    )
+    res.json(savedVersion)
+  },
+  
 
 };
--- server/routes/api/deck-routes.js
+++ server/routes/api/deck-routes.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
-const { getUserDecks, newVersion, createDeck, updateDeck, saveDeckVersion } = require('../../controllers/deck-controller');
+const { getUserDecks, createDeck, addToDeck, saveCurrentVersion } = require('../../controllers/deck-controller');
 
 //api/decks
 
 router.route('/').get(getUserDecks);  //get decks by user id
-router.route('/newversion').put(newVersion);  //create new version of deck
-router.route('/create').post(createDeck);  //create all new deck
-router.route('/update').put(updateDeck);   //add card to deck or sideboard
-router.route('/save').put(saveDeckVersion);  //save version of deck
+router.route('/create').post(createDeck);  //create all new deck //create new version of deck
+router.route('/addCard').put(addToDeck);   //add card to deck or sideboard
+router.route('/save').put(saveCurrentVersion);  //save version of deck
 // change deckName, change wins/losses
 //remove card from deck, 
 //mark card as commander, 
