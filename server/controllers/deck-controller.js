7420678956d180bff87e5391d44b0189d84aa1fd
Cygnet717 <kathybradt717@gmail.com>
2021-12-07 12:48:34 -0600

    added remove card route and started remove deck route

--- server/controllers/deck-controller.js
+++ server/controllers/deck-controller.js
@@ -4,65 +4,138 @@ const { Deck, User } = require('../models');
 module.exports = {
   //get all decks by user
   async getUserDecks(req, res) { // send "user_id" in body
-    const foundDecks = await User.findOne(
-      {_id: req.body.user_id}
-    )
-      .populate('decks');
+    try{
+      const foundDecks = await User.findOne(
+        {_id: req.body.user_id}
+      )
+        .populate('decks');
 
-    res.json(foundDecks.decks);
+      res.json(foundDecks.decks);
+    } catch (err){
+      res.status(400).json(err)
+    }
   },
 
   //make new deck
       // send "deckName", "user_id", "format"
   //or to make new version
-      // send "deckName", "user_id", "versionToBe"++, "format", "deckCards" and "sideboard" array minus "_id" for each card
+      // send "deckName", "user_id", "versionToBe"++, "format", "deckCards" and "sideBoard" array minus "_id" for each card
   async createDeck(req, res) {  
-    const newDeck = await Deck.create(req.body)
- 
-    const userOfDeck = await User.findOneAndUpdate(
-      {_id: req.body.user_id},
-      {$push: {decks: newDeck._id}},
-      {new: true}
-      )
+    try{
+      const newDeck = await Deck.create(req.body)
+  
+      const userOfDeck = await User.findOneAndUpdate(
+        {_id: req.body.user_id},
+        {$push: {decks: newDeck._id}},
+        {new: true}
+        )
 
-    res.json(userOfDeck)
+      res.json(userOfDeck)
+    } catch (err){
+      res.status(400).json(err)
+    }
   },
 
-  //add card to deck/sideboard
-  async addToDeck(req, res) {  // send "deck_id", "cardName", "image", "cardType", "commander"boolean, "sideboard"boolean
+  //add card to deck/sideBoard
+  async addToDeck(req, res) {  // send "deck_id", "cardName", "image", "cardType", "commander"boolean, "sideBoard"boolean
     let updatedDeck;
-    const newCard = {
-      cardName: req.body.cardName,
-      image: req.body.image,
-      cardType: req.body.cardType,
-      commander: req.body.commander
+    try{
+      const newCard = {
+        cardName: req.body.cardName,
+        image: req.body.image,
+        cardType: req.body.cardType,
+        commander: req.body.commander
+      }
+      if(req.body.sideBoard){
+        updatedDeck = await Deck.findOneAndUpdate(
+          {_id: req.body.deck_id},
+          {$push: {sideBoard: newCard}},
+          {new: true}
+        )
+      } else {
+        updatedDeck = await Deck.findOneAndUpdate(
+          {_id: req.body.deck_id},
+          {$push: {deckCards: newCard}},
+          {new: true}
+        )
+      }
+      res.json(updatedDeck)
+    } catch (err){
+      res.status(400).json(err)
     }
-    if(req.body.sideboard){
-      updatedDeck = await Deck.findOneAndUpdate(
-        {_id: req.body.deck_id},
-        {$push: {sideBoard: newCard}},
-        {new: true}
-      )
-    } else {
-      updatedDeck = await Deck.findOneAndUpdate(
-        {_id: req.body.deck_id},
-        {$push: {deckCards: newCard}},
-        {new: true}
-      )
-    }
-    
-    res.json(updatedDeck)
   },
 
   //save current version of deck
   async saveCurrentVersion(req, res) {  // send "deck_id", "versionToBe"
-    const savedVersion = await Deck.findOneAndUpdate(
-      {_id: req.body.deck_id},
-      {version: req.body.versionToBe},
-      {new: true}
-    )
-    res.json(savedVersion)
+    try{
+      const savedVersion = await Deck.findOneAndUpdate(
+        {_id: req.body.deck_id},
+        {version: req.body.versionToBe},
+        {new: true}
+      )
+      res.json(savedVersion)
+    } catch (err){
+      res.status(400).json(err)
+    }
   },
   
+  //update deckName, wins/losses
+  async updateDeck(req, res) {  // send "deck_id", "deckName"  OR "deck_id", "wins", "losses"
+    let updatedDeck;
+    try{
+      if(req.body.deckName){
+        updatedDeck = await Deck.findOneAndUpdate(
+          {_id: req.body.deck_id},
+          {deckName: req.body.deckName},
+          {new: true}
+        )
+      } else {
+        updatedDeck = await Deck.findOneAndUpdate(
+          {_id: req.body.deck_id},
+          {wins: req.body.wins, losses: req.body.losses},
+          {new: true}
+        )
+      }
+      res.json(updatedDeck)
+    } catch (err){
+      res.status(400).json(err)
+    }
 
+  },
+
+  //remove card from deck
+  async removeCard (req, res) {  // send "deck_id", "sideBoard"boolean, "card_id"
+    let deletedCard;
+    try{
+      if(req.body.sideBoard){
+        deletedCard = await Deck.findOneAndUpdate(
+          {_id: req.body.deck_id}, 
+          {$pull: {sideBoard: {_id: req.body.card_id}}},
+          { new: true }
+        )
+      } else {
+        deletedCard = await Deck.findOneAndUpdate(
+          {_id: req.body.deck_id}, 
+          {$pull: {deckCards: {_id: req.body.card_id}}},
+          { new: true }
+        )
+      }
+      res.json(deletedCard)
+    } catch (err){
+      res.status(400).json(err)
+    }
+  },
+
+  //delete deck
+  async removeDeck (req, res) {  //send deck_id parameter in url 
+    try{  //cant send a body in a delete request
+      const updatedUser = await User.findOneAndUpdate(
+        //somehow need to find user and remove deck_id from their deck list
+      )
+      const deletedDeck = await Deck.remove({_id: req.params.deck_id})
+      res.json(deletedDeck)
+    } catch (err){
+      res.status(400).json(err)
+    }
+  }
 };
--- server/routes/api/deck-routes.js
+++ server/routes/api/deck-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getUserDecks, createDeck, addToDeck, saveCurrentVersion } = require('../../controllers/deck-controller');
+const { getUserDecks, createDeck, addToDeck, saveCurrentVersion, updateDeck , removeCard, removeDeck} = require('../../controllers/deck-controller');
 
 //api/decks
 
@@ -7,8 +7,9 @@ router.route('/').get(getUserDecks);  //get decks by user id
 router.route('/create').post(createDeck);  //create all new deck //create new version of deck
 router.route('/addCard').put(addToDeck);   //add card to deck or sideboard
 router.route('/save').put(saveCurrentVersion);  //save version of deck
-// change deckName, change wins/losses
-//remove card from deck, 
+router.route('/update').put(updateDeck)  // change deckName, change wins/losses
+router.route('/removeCard').put(removeCard) //remove card from deck, 
+router.route('/removeDeck').delete(removeDeck) //delete deck
 //mark card as commander, 
 //add comment on deck
 module.exports = router;
