f283d606aec38f94874ec89877a06aae14901c2c
Cygnet717 <kathybradt717@gmail.com>
2022-01-04 13:29:17 -0600

    styilng for home page and basic search

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Home.css';
+import { Form, Button, FormControl } from 'react-bootstrap';
 import Card from '../../components/card/Card'
 
 export default function Home() {
+  const [advSearch, setAdvSearch] = useState(false)
+  //card name, color, cardtype
+  
   return (
     <>
-    <div className="container">
-      <h1>Home</h1>
-      <p>search stuff here</p>
-      <form>
-        <input type="text" name="searchField" placeholder="Search"/>
-        <button type="submit">Search</button>
-      </form>
+    <div>
+      <Form className='searchForm'>
+        <div className='searchSection'>
+          <Form.Select aria-label="select card type" className='fitContent'>
+            <option>Card Type</option>
+            <option value="creature">Creature</option>
+            <option value="sorcery">Sorcery</option>
+            <option value="instant">Instant</option>
+          </Form.Select>
+          <FormControl
+            placeholder="Card Name"
+            className='fitContent'
+            aria-label="card name"
+            aria-describedby="basic-addon1"
+          />
+        </div>
+        <div className='searchSection'>
+          <Form.Check type="checkbox" id="custom-checkbox" label="White" className='color white'/> 
+          <Form.Check type="checkbox" id="custom-checkbox" label="Blue" className='color blue'/> 
+          <Form.Check type="checkbox" id="custom-checkbox" label="Black" className='color black'/> 
+          <Form.Check type="checkbox" id="custom-checkbox" label="Red" className='color red'/> 
+          <Form.Check type="checkbox" id="custom-checkbox" label="Green" className='color green'/> 
+          <Form.Check type="checkbox" id="custom-checkbox" label="Colorless" className='color colorless'/> 
+        </div>
+        {advSearch? 
+          <>
+            More Search Options
+            <Button onClick={() => setAdvSearch(!advSearch)}>Advanced Search</Button>
+          </>
+          :
+          <Button onClick={() =>setAdvSearch(!advSearch)}>Advanced Search</Button>
+        }
+        <Button type="submit">Search</Button>
+      </Form>
     </div>
+
     <div className="resultsContainer">
       <Card/>
       <Card/>
