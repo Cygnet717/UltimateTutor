a90a6d69408b13bc2c0288c0292a6dd0f67aabe8
Cygnet717 <kathybradt717@gmail.com>
2022-01-11 10:55:37 -0600

    click to show both sides of double sided card

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -5,18 +5,34 @@ import cardImage from '../../images/Magic_card_back.jpg'
 export default function Card(props) {
   let frontSideImage = cardImage;
   let backSideImage = cardImage;
+  let isDoubleSided = false;
   if(props.cardData){
     if(props.cardData.image_uris){
       frontSideImage = props.cardData.image_uris.small
     }else{
+      isDoubleSided = true;
       frontSideImage = props.cardData.card_faces[0].image_uris.small;
       backSideImage = props.cardData.card_faces[1].image_uris.small;
+    }
+  }
+
+  function flipCard (e){
+    let currentImage = e.currentTarget.src
+    if(currentImage === frontSideImage){
+      e.currentTarget.src = backSideImage
+    } else {
+      e.currentTarget.src = frontSideImage
     }
   }
   return (
     <div className="singleCard">
       <p>{props.cardData? props.cardData.name : "Card Name"}</p>
-      <img className='cardImage' alt={props.cardData? props.cardData.name : "Card Name"} src={frontSideImage}/>
+      {isDoubleSided? 
+        <img className='cardImage' alt={props.cardData? props.cardData.name : "Card Name"} src={frontSideImage} onClick={e => flipCard(e)}/>
+        :
+        <img className='cardImage' alt={props.cardData? props.cardData.name : "Card Name"} src={frontSideImage}/>
+      }
+
       <form>
         <select>
           <option>Pick Deck</option>
