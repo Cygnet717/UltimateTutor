0f1b5df175a140e271ad2eb354f365727b5a4c3e
Cygnet717 <kathybradt717@gmail.com>
2022-01-12 13:35:52 -0600

    delete deck button woking but not refreshing page

--- client/src/components/deskfeed/DeskFeed.js
+++ client/src/components/deskfeed/DeskFeed.js
@@ -2,13 +2,13 @@ import React, { useContext, useState } from 'react';
 import {v4 as uuid} from 'uuid';
 import {Form, Button, Container, Row, Col} from 'react-bootstrap';
 import {AuthContext} from "../../context/AuthContext";
-import {createDeck} from '../../utils/deckApi';
+import {createDeck, removeDeck} from '../../utils/deckApi';
+import bin from '../../images/bin.png'
 import './DeskFeed.css';
 
 export default function DeskFeed() {
   const {user} = useContext(AuthContext)
   const {userDecks} = useContext(AuthContext)
-  const [currentDecks, setCurrentDecks] = useState()
   const [newDeckData, setNewDeckData] = useState()
   console.log(userDecks)
 
@@ -18,8 +18,6 @@ export default function DeskFeed() {
   };
 
   const createNewDeck = async (e) => {
-    // e.preventDefault()
-    
     try{
       const response = await createDeck(newDeckData)
 
@@ -34,30 +32,51 @@ export default function DeskFeed() {
     setNewDeckData({user_id: user.data._id, deckName: '', format: ''})
   }
 
+  const startDeleteDeck = async (e) => {
+    const deck_id = e.currentTarget.dataset.deck_id
+    const user_id = e.currentTarget.dataset.user_id
+    console.log('refresh')
+    // try{
+    //   const response = await removeDeck(deck_id, user_id)
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
+    //   const deleted = await response.json()
+    //   console.log(deleted)
+    // }catch(err){
+    //   console.error(err)
+    // }
+  }
+
   return (
     <div className="deskFeed">
       {userDecks.map(deck => {
         let date = new Date(deck.dateStarted).toLocaleDateString()
         let commander = deck.deckCards.find(card => card.commander )
         return(
-          <div className='singleDeck' key={uuid()}>
-            <div className='deckSection'>
-              <h4>{deck.deckName}</h4>
-              <div>Card Count: {deck.deckCards.length}</div>
-              <div>Side Board: {deck.sideBoard.length}</div>
-              <div>Version: {deck.versionToBe -1}</div>
-            </div>
-            <div className='deckSection'>
-              <div>Date Created: {date}</div>
-              {deck.format === 'commander'?
-                (
-                  <p>Commander: {commander.cardName}</p>
-                ):(
-                  <p>other</p>
-                )
-              }
-            </div>
-          </div>
+          <Container className='singleDeck' key={uuid()}>
+            <Col>
+              <Container >
+                  <Col>{deck.deckName}</Col>
+                  <Col>Card Count: {deck.deckCards.length}</Col>
+                  <Col>Side Board: {deck.sideBoard.length}</Col>
+                  <Col>Version: {deck.versionToBe -1}</Col>
+              </Container>
+              <Container>
+                  <Col>Date Created: {date}</Col>
+                  {deck.format === 'commander'?
+                    (
+                      <Col>Commander: {commander? commander.cardName : '--'}</Col>
+                    ):(
+                      <Col>Format: {deck.format}</Col>
+                    )
+                  }
+              </Container>
+            </Col>
+            <Col className='deleteButton' xs={1}>
+              <button  data-deck_id={deck._id} data-user_id={deck.user_id} onClick={startDeleteDeck}><img src={bin} alt='delete button'/></button>
+            </Col>
+          </Container>
         )
       })}
       <div className='singleDeck'>
@@ -73,9 +92,9 @@ export default function DeskFeed() {
                 <Form.Group className="mb-3">
                   <Form.Select id="Format" name="format" onChange={handleInputChange}>
                     <option>Select Format</option>
-                    <option value='Standard'>Standard</option>
-                    <option value='Commander'>Commander</option>
-                    <option value='Historic'>Historic</option>
+                    <option value='standard'>Standard</option>
+                    <option value='commander'>Commander</option>
+                    <option value='historic'>Historic</option>
                   </Form.Select>
                 </Form.Group>
               </Col>
