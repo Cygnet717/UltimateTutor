68077534e5bf144f67cc0a55caed587d00cd9993
Cygnet717 <kathybradt717@gmail.com>
2022-01-24 14:18:16 -0600

    rendering dropdown for picking commander and making a card commander is working, displaying comandername is not

--- client/src/pages/deckfeed/DeckFeed.js
+++ client/src/pages/deckfeed/DeckFeed.js
@@ -3,6 +3,7 @@ import {useState, useEffect, useContext} from 'react'
 import {AuthContext} from "../../context/AuthContext"
 import {useParams} from 'react-router-dom';
 import cardImage from '../../images/Magic_card_back.jpg'
+import { toggleCommander } from '../../utils/deckApi';
 import { v4 as uuidv4 } from 'uuid';
 import './DeckFeed.css';
 
@@ -14,13 +15,15 @@ export default function DeckFeed() {
   const [displayedCard, setDisplayedCard] = useState(cardImage)
   let commanderCard;
 
+  if(deckData.format === 'Comander'){
+    console.log('its a commander')
+    commanderCard = deckData.deckCards.find(card => card.commander )
+    console.log(commanderCard)
+  }
+
   const getDetails = async(deck_id) => {
     const currentDeck = userDecks.find(deck => deck._id === deck_id)
-    if(currentDeck.format === 'Comander'){
-      console.log('its a commander')
-      commanderCard = currentDeck.deckCards.find(card => card.commander )
-      console.log(commanderCard)
-    }
+    
     if(currentDeck){  //['Creature', 'Instant', 'Sorcery', 'Enchantment', 'Land', 'Planeswalker', 'Artifact']
       let deckCreatures = [];
       let deckInstants = [];
@@ -43,7 +46,7 @@ export default function DeckFeed() {
             break;
           case 'Enchantment':
             deckEnchantments.push(currentDeck.deckCards[i])
-            break;
+            break; 
           case 'Land':
             deckLands.push(currentDeck.deckCards[i])
             break;
@@ -67,9 +70,18 @@ export default function DeckFeed() {
         {Artifacts: deckArtifacts}
       ])
       setDeckData(currentDeck)
-
     }
-   
+
+
+  }
+
+  const handleCommanderSelect = (event) => {
+    const commanderData = {
+      deck_id: deckData._id,
+      card_id: event.target.value,
+      commander: true
+    }
+    toggleCommander(commanderData)
   }
 
   useEffect(() => {
@@ -85,11 +97,14 @@ export default function DeckFeed() {
         {deckData.format === 'Commander'?
           <div>
             Commander: {
-              commanderCard.cardName ? 
+              commanderCard ? 
               commanderCard.cardName 
               : 
-              <select>
-                
+              <select onChange={handleCommanderSelect}>
+                <option>select a commander</option>
+                 {deckData.deckCards.map(card => 
+                   <option key={uuidv4()} value={card._id}>{card.cardName}</option>
+                 )}
               </select>
               }
           </div>
@@ -119,11 +134,11 @@ export default function DeckFeed() {
         {sortedCards.map(type => {
           if(Object.values(type)[0].length > 0){
             return(
-              <div key={uuidv4}> 
+              <div key={uuidv4()}> 
                 <h4> {Object.keys(type)[0]} ({Object.values(type)[0].length})</h4>
                 <ul>
                   {Object.values(type)[0].map(card => 
-                    <li key={uuidv4} onMouseOver={() => setDisplayedCard(card.image.front)}>{card.cardName}</li>
+                    <li key={uuidv4()} onMouseOver={() => setDisplayedCard(card.image.front)}>{card.cardName}</li>
                   )}
                 </ul>
               </div>
