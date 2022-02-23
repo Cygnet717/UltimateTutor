45e34bfe4d301bd75aed284a746e9a577a77d8a0
Cygnet717 <kathybradt717@gmail.com>
2022-02-23 11:52:24 -0600

    fixed db issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-console.log(process.env.MONGODB_URI)
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ultimateTutorDB', {});
 
 module.exports = mongoose.connection;
