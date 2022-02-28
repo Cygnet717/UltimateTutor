4f505447fd928246b93e7da9a62955ff715d3e5c
Cygnet717 <kathybradt717@gmail.com>
2022-02-28 11:23:52 -0600

    switched env back

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://kjb717:newDb7!7@clusterkb.vc8qt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ultimateTutorDB', {});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ultimateTutorDB', {});
 
 module.exports = mongoose.connection;
