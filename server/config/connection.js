e14ca8958de35faae932fd37bf3c29064e759f96
Cygnet717 <kathybradt717@gmail.com>
2022-02-28 11:13:28 -0600

    checking mongodb_uri environmental variable

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ultimateTutorDB', {});
+mongoose.connect('mongodb+srv://kjb717:newDb7!7@clusterkb.vc8qt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ultimateTutorDB', {});
 
 module.exports = mongoose.connection;
